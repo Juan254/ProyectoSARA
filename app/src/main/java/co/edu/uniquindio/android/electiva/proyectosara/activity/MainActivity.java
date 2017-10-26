cc269eb95c366d3eae8cae656aeb941f08f91fab
amfajardoo <andres.mauricio94@hotmail.com>
2017-10-26 12:04:37 -0500

    creacion de las carpetas vo, activity, fragments y util

--- app/src/main/java/co/edu/uniquindio/android/electiva/proyectosara/activity/MainActivity.java
+++ app/src/main/java/co/edu/uniquindio/android/electiva/proyectosara/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package co.edu.uniquindio.android.electiva.proyectosara;
+package co.edu.uniquindio.android.electiva.proyectosara.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import co.edu.uniquindio.android.electiva.proyectosara.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/proyectosara/vo/Monitor.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.android.electiva.proyectosara.vo;
+
+/**
+ * Created by andre on 26/10/2017.
+ */
+
+public class Monitor {
+}
