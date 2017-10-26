6458f7ec7fd9ec00fa15cd716188b4aeed3aa567
amfajardoo <andres.mauricio94@hotmail.com>
2017-10-26 16:24:53 -0500

    se crearon los fragmentos lista de monitor y el xml resumen monitor

---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/proyectosara/fragments/ListaDeMonitoresFragment.java
@@ -0,0 +1,105 @@
+package co.edu.uniquindio.android.electiva.proyectosara.fragments;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+import co.edu.uniquindio.android.electiva.proyectosara.R;
+import co.edu.uniquindio.android.electiva.proyectosara.util.AdaptadorMonitor;
+import co.edu.uniquindio.android.electiva.proyectosara.vo.Monitor;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListaDeMonitoresFragment extends Fragment implements AdaptadorMonitor.onClickAdaptadorMonitor {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Monitor> monitores;
+    private AdaptadorMonitor adaptadorMonitor;
+    private onMonitorSeleccionadoListener listener;
+
+    public ListaDeMonitoresFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if(context instanceof Activity){
+            Activity activity = (Activity) context;
+            listener = (onMonitorSeleccionadoListener) activity;
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        recyclerView = (RecyclerView) getView().findViewById(R.id.listaMonitores);
+
+        adaptadorMonitor = new AdaptadorMonitor(monitores, this);
+        recyclerView.setAdapter(adaptadorMonitor);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayout.VERTICAL, false));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lista_de_monitores, container, false);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClickPosicion(int posicion) {
+
+        listener.onMonitorSeleccionado(posicion);
+
+    }
+
+    public interface onMonitorSeleccionadoListener{
+        void onMonitorSeleccionado(int posision);
+    }
+
+    public ArrayList<Monitor> getMonitores() {
+        return monitores;
+    }
+
+    public void setMonitores(ArrayList<Monitor> monitores) {
+        this.monitores = monitores;
+    }
+}
--- app/src/main/java/co/edu/uniquindio/android/electiva/proyectosara/util/AdaptadorMonitor.java
+++ app/src/main/java/co/edu/uniquindio/android/electiva/proyectosara/util/AdaptadorMonitor.java
@@ -1,16 +1,85 @@
 package co.edu.uniquindio.android.electiva.proyectosara.util;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import co.edu.uniquindio.android.electiva.proyectosara.R;
+import co.edu.uniquindio.android.electiva.proyectosara.fragments.ListaDeMonitoresFragment;
+import co.edu.uniquindio.android.electiva.proyectosara.vo.Monitor;
 
 /**
  * Created by andre on 26/10/2017.
  */
 
-public class AdaptadorMonitor {
+public class AdaptadorMonitor extends RecyclerView.Adapter<AdaptadorMonitor.MonitorViewHolder> {
+
+    private ArrayList<Monitor> monitores;
+    private onClickAdaptadorMonitor listener;
 
 
+    public AdaptadorMonitor(ArrayList<Monitor> monitores, ListaDeMonitoresFragment listaDeMonitoresFragment){
 
-    public class MonitorViewHolder {
+        this.monitores = monitores;
+        listener = (onClickAdaptadorMonitor) listaDeMonitoresFragment;
 
+    }
+
+    @Override
+    public MonitorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.resumen_monitor, parent, false);
+        MonitorViewHolder monitorViewHolder = new MonitorViewHolder(view);
+
+        return monitorViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MonitorViewHolder holder, int position) {
+        holder.bindMonitor(monitores.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return monitores.size();
+    }
+
+    public class MonitorViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView txtNombre;
+        private TextView txtLineaMonitoria;
+
+        public MonitorViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+            txtNombre = (TextView) itemView.findViewById(R.id.nombreMonitor);
+            txtLineaMonitoria = (TextView) itemView.findViewById(R.id.lineaMonitoria);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            listener.onClickPosicion(getAdapterPosition());
+        }
+
+        public void bindMonitor(Monitor monitor){
+
+
+            txtNombre.setText(monitor.getNombre());
+            txtLineaMonitoria.setText(monitor.getLineaMonitoria());
+
+        }
+    }
+
+    public interface onClickAdaptadorMonitor {
+        void onClickPosicion(int posicion);
     }
 }
